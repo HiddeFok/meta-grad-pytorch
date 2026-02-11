86243a5bcd4d707100221dd85fa5bb3121e6b32a
HiddeFok <hidde.fokkema@gmail.com>
2026-02-11 17:33:02 +0100

    slowly getting what needs to be done

--- metagrad/metagrad.py
+++ metagrad/metagrad.py
@@ -2,10 +2,30 @@ import torch
 from torch.optim import Optimizer
 
 class MetaGrad(Optimizer):
-    def __init__(self, params, defaults):
-        super().__init__(params, defaults)
+    def __init__(self, params, sigma: float = 1.0, D_inf: float = 1.0):
+        defaults = dict(sigma=sigma, D_inf = D_inf)
+        super(MetaGrad, self).__init__(params, defaults)
+
+        for group in self.param_groups:
+            for p in group['params']:
+                self.state[p]["w_etas"] = {}
+                self.state[p]["B_prev"] = 0.0
+
+    def _clip_gradients(self, grad, B_current, B_prev):
+        return grad.mul_(B_prev / B_current)
 
     def step(self, closure=None):
         loss = None
         if closure is not None:
             loss = closure()
+
+
+        for group in self.param_groups:
+            sigma = group['sigma']
+            D_inf = group['D_inf']
+            for p in group['params']:
+                loss = closure()
+
+            state = self.state[p]
+            if len(state) == 0:
+                state['w_etas'][] = torch.zeros_like()
